6923d89203028630701bb0b94b43a3687f6cb932
Catrionic <80280006+Catrionic@users.noreply.github.com>
2021-07-28 15:12:24 +0200

    nowy allah

--- edziennik/edziennik/urls.py
+++ edziennik/edziennik/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),dsauhfadsffgdh
+    path('admin/', admin.site.urls),dsauhfadsffgdh asdfas
 ]
