81337b438912d061a46260f8e76207be221b95b7
Catrionic <80280006+Catrionic@users.noreply.github.com>
2021-08-11 19:29:55 +0200

    HEHEHUHE

--- edziennik/dziennik/generic_views.py
+++ edziennik/dziennik/generic_views.py
@@ -33,12 +33,25 @@ class UczniowieListView(ListView, LoginRequiredMixin):
         context["students"] = Student.objects.filter(klasa__id=kwargs['object'].id)
         return context
 
+class Przedmiot2TemplateView(TemplateView):
+    template_name = "przedmioty.html"
+    model = Przedmiot
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        print(kwargs)
+        student = Student.objects.get(id=kwargs['pk'])
+        klasa = student.klasa
+        context["przedmioty"] = Przedmiot.objects.filter(klasa__id=klasa.id)
+        return context
+
+class OcenaTemplateView(TemplateView):
+    template_name = "ocena.html"
+    model = Ocena
 
 
 class PrzedmiotyDetailView(DetailView, LoginRequiredMixin):
     model = Przedmiot
     template_name ="przedmioty_detail_views.html"
-
 
 
 class KlasaListView(ListView):
@@ -95,4 +108,64 @@ class PrzedmiotDeleteView(DeleteView):
 	model = Przedmiot
 	template_name ="przedmiot_delete_form.html"
 	success_url = reverse_lazy('generic_urls:przedmiot-list-view')
+
+class NauczycielListView(ListView):
+    template_name = "nauczyciele.html"
+    model = Nauczyciel
+
+
+class NauczycielTemplateView(TemplateView):
+    template_name = "nauczyciele.html"
+    extra_context = {'nauczyciele': Nauczyciel.objects.all()}
+
+
+class NauczycielCreateView(CreateView):
+    model = Nauczyciel
+    template_name ="form_nauczyciel.html"
+    fields = "__all__"
+    success_url = reverse_lazy('generic_urls:nauczyciel-template-view')
+
+
+class NauczycielUpdateView(UpdateView):
+	model = Nauczyciel
+	fields = ("name")
+	template_name = "form_nauczyciel.html"
+	success_url = reverse_lazy("edziennik:nauczyciel")
+
+
+class NauczycielDeleteView(DeleteView):
+	model = Nauczyciel
+	template_name ="nauczyciel_delete_form.html"
+	success_url = reverse_lazy('generic_urls:nauczyciel-list-view')
+
+
+class StudentlListView(ListView):
+    template_name = "student.html"
+    model = Student
+
+
+class StudentTemplateView(TemplateView):
+    template_name = "student.html"
+    extra_context = {'students': Student.objects.all()}
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    template_name ="form_student.html"
+    fields = "__all__"
+    success_url = reverse_lazy('generic_urls:student-template-view')
+
+
+class StudentUpdateView(UpdateView):
+	model = Student
+	fields = ("name")
+	template_name = "form_student.html"
+	success_url = reverse_lazy("edziennik:student")
+
+
+class StudentDeleteView(DeleteView):
+	model = Student
+	template_name ="student_delete_form.html"
+	success_url = reverse_lazy('generic_urls:student-template-view')
+
 
