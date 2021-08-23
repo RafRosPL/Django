9535b431e87c2c27037a66c98414ba7d11905992
Catrionic <80280006+Catrionic@users.noreply.github.com>
2021-08-23 13:43:09 +0200

    speedzio2

--- e3work/users/views.py
+++ e3work/users/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from Django.e3work.users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-
+    permission_classes = [permissions.IsAuthenticated]
