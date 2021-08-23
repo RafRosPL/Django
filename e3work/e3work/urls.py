c025b47066bdbeb8c1bea82fdbe0c563bcccf3c6
Catrionic <80280006+Catrionic@users.noreply.github.com>
2021-08-23 18:32:53 +0200

    git

--- e3work/e3work/urls.py
+++ e3work/e3work/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from Django.e3work.users import views
+from users import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
